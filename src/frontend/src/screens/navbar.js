b098fd1b0c43091830ed12b049eb48928d446db2
puppetmaster5x1 <puppetmaster5x1@gmail.com>
2023-02-08 19:44:07 +0530

    deployment test

--- src/frontend/src/screens/navbar.js
+++ src/frontend/src/screens/navbar.js
@@ -25,7 +25,7 @@ class NavigBar extends Component {
       <div>
 
         <Navbar className="navbar-dark fixed-top" style={{backgroundColor:'black',height:"100px"}} expand="md">
-          <Navbar.Brand className="fs-1" href="/" style={{fontSize:30}}>SPORTIN</Navbar.Brand>
+          <Navbar.Brand className="fs-1" href="/" style={{fontSize:30,paddingLeft:"2rem"}}>SPORTIN</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse className="" id="basic-navbar-nav">
             <div className="m-auto">
@@ -46,24 +46,24 @@ class NavigBar extends Component {
                     <NavDropdown.Item href="#">Video Games</NavDropdown.Item>
                 
                 </NavDropdown>
-                <NavDropdown title={<span className="text-dark">Sports</span>} href="#">Sports</NavDropdown>
+                <NavDropdown title={<span className="text-dark">Sports</span>} href="#" >Sports</NavDropdown>
                 
               </NavDropdown>
 
-              <NavDropdown  title={<span className="text-light">Support</span>} id="basic-nav-dropdown" style={{color:"white",backgroundColor:this.getColor(this.state.innerWidth),width:"100%"}}>
+              <NavDropdown  title={<span className="text-light">Support</span>} id="basic-nav-dropdown" style={{color:"white",backgroundColor:this.getColor(this.state.innerWidth),width:"100%",paddingRight:"1rem",paddingLeft:"1rem"}}>
                 <NavDropdown.Item href="#">Action</NavDropdown.Item>
                 <NavDropdown.Item href="#">Another action</NavDropdown.Item>
                 <NavDropdown.Divider />
                 <NavDropdown.Item href="#">Something else here</NavDropdown.Item>
               </NavDropdown>
 
-              <Nav.Link className="nav-item" href="/build" style={{color:"white",backgroundColor:this.getColor(this.state.innerWidth),width:"100%"}} id="basic-nav-dropdown" >Build</Nav.Link>
+              <Nav.Link className="nav-item" href="/build" style={{color:"white",backgroundColor:this.getColor(this.state.innerWidth),width:"100%",marginRight:"2rem"}} id="basic-nav-dropdown" >Build</Nav.Link>
 
-              <LinkContainer to='/cart' style={{color:"white",backgroundColor:this.getColor(this.state.innerWidth),width:"100%"}}>
-                <Nav.Link ><i className="fas fa-shopping-cart"></i></Nav.Link>
+              <LinkContainer to='/cart' style={{color:"white",backgroundColor:this.getColor(this.state.innerWidth),width:"100%",paddingRight:"0.5rem"}}>
+                <Nav.Link ><i className="fas fa-shopping-cart"></i>Cart</Nav.Link>
               </LinkContainer>
               
-              <Nav.Link className="nav-item" href="/profile" style={{color:"white",backgroundColor:this.getColor(this.state.innerWidth),width:"100%"}} id="basic-nav-dropdown" >Account</Nav.Link>
+              <Nav.Link className="nav-item" href="/profile" style={{color:"white",backgroundColor:this.getColor(this.state.innerWidth),width:"100%",paddingLeft:"0.5rem",marginRight:"2rem"}} id="basic-nav-dropdown" >Account</Nav.Link>
             </Nav>
             
           </Navbar.Collapse>
